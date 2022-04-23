41b1a4ade4dfaca8d61d08c2aee1059d78ebe357
akemimadoka <chino@hotococoa.moe>
2022-04-23 18:58:13 +0800

    修改输出格式，添加可选根据 hash 迁移翻译的功能

--- UmamusumeStoryDataExtractor/Program.fs
+++ UmamusumeStoryDataExtractor/Program.fs
@@ -3,6 +3,7 @@
 open System
 open System.IO
 open System.Text.Json
+open System.Text.Json.Serialization
 open System.Text.Encodings.Web
 open System.Text.RegularExpressions
 open System.Threading
@@ -27,6 +28,43 @@ type TextSource =
         | Story textData -> textData
         | Race textData -> textData
 
+type TextBlock = {
+    Name: string
+    Text: string
+    ChoiceDataList: string[]
+    ColorTextInfoList: string[]
+}
+
+[<JsonConverter(typeof<TextDataConverter>)>]
+type TextData =
+    | StoryData of Title: string * TextBlockList: ValueOption<TextBlock>[]
+    | RaceData of textData: string[]
+
+and TextDataConverter() =
+    inherit JsonConverter<TextData>()
+    
+    override _.Read(reader: byref<Utf8JsonReader>, typeToConvert: Type, options: JsonSerializerOptions): TextData =
+        raise (NotImplementedException())
+
+    override _.Write(writer: Utf8JsonWriter, value: TextData, options: JsonSerializerOptions) =
+        match value with
+            | StoryData (title, textBlockList) ->
+                writer.WriteStartObject()
+                writer.WriteString("Title", title)
+                writer.WritePropertyName("TextBlockList")
+                writer.WriteStartArray()
+                for textBlock in textBlockList do
+                    match textBlock with
+                    | ValueSome block -> JsonSerializer.Serialize(writer, block, options)
+                    | ValueNone -> writer.WriteNullValue()
+                writer.WriteEndArray()
+                writer.WriteEndObject()
+            | RaceData textData ->
+                writer.WriteStartArray()
+                for text in textData do
+                    writer.WriteStringValue(text)
+                writer.WriteEndArray()
+
 module Program =
     let StoryTimelinePattern =
         Regex(@"story/data/\d+/\d+/storytimeline_\d+", RegexOptions.Compiled)
@@ -47,13 +85,34 @@ module Program =
 
     [<EntryPoint>]
     let main args =
-        if args.Length <> 2 then
-            Console.WriteLine "Usage: UmamusumeStoryDataExtractor <UmamusumeGameDataDirectoryPath> <OutputPath>"
+        if args.Length < 2 || args.Length > 3 then
+            Console.WriteLine "Usage: UmamusumeStoryDataExtractor <UmamusumeGameDataDirectoryPath> <OutputPath> [HashJsonDirectory]"
             1
         else
             let dataDir = args[0]
             let outputDir = args[1]
+            let hashMap =
+                if args.Length = 3 then
+                    let mergedMap = Collections.Generic.Dictionary<uint64, string>()
+                    let maps =
+                        Directory.EnumerateFiles(args[2], "*.json", SearchOption.AllDirectories)
+                        |> Seq.map(fun file ->
+                            use fileStream = new FileStream(file, FileMode.Open, FileAccess.Read)
+                            JsonSerializer.Deserialize<Collections.Generic.Dictionary<string, string>>(fileStream)
+                        )
+                    Seq.fold(fun (resultMap: Collections.Generic.Dictionary<uint64, string>) (map: Collections.Generic.Dictionary<string, string>) ->
+                        for kv in map do
+                            let succeed, hash = UInt64.TryParse(kv.Key)
+                            if succeed then
+                                resultMap.TryAdd(hash, kv.Value) |> ignore
+                            else
+                                ()
+                        resultMap) mergedMap maps
+                else
+                    null
 
+            let mayLocalize str = if hashMap = null then str else let found, var = hashMap.TryGetValue(CppUtility.GetCppStdHash(str)) in if found then var else str
+                    
             if Directory.Exists(dataDir) then
                 let textSources = GetTextSourcePaths dataDir |> Seq.toArray
                 Console.WriteLine $"Discovered {textSources.Length} stories"
@@ -69,7 +128,7 @@ module Program =
                         MaxDegreeOfParallelism = Environment.ProcessorCount
                     )
 
-                Console.CancelKeyPress.AddHandler (fun sender e ->
+                Console.CancelKeyPress.AddHandler (fun _ e ->
                     cancellationTokenSource.Cancel()
                     e.Cancel <- true)
 
@@ -97,69 +156,67 @@ module Program =
                                     progressBar.Tick($"Skipping not downloaded {data.Name}.json")
                                 else
                                     assetManager.LoadFiles(path)
-
+                                    
                                     let loadedFile = assetManager.assetsFileList[0]
 
-                                    let textDataSeq =
-                                        loadedFile.Objects
+                                    let mutable mainScript: Collections.Specialized.OrderedDictionary = null
+                                    let textClipData = Collections.Generic.Dictionary<int64, Collections.Specialized.OrderedDictionary>()
+
+                                    for o in loadedFile.Objects
                                         |> Seq.filter (fun o -> o :? MonoBehaviour)
                                         |> Seq.cast<MonoBehaviour>
-                                        |> Seq.map (fun o ->
-                                            if o.m_Name = mainScriptName then
-                                                let typeTree = o.ToType()
+                                        do
+                                        if o.m_Name = mainScriptName then
+                                            mainScript <- o.ToType()
+                                        else
+                                            let succeed, script = o.m_Script.TryGet()
+                                            if succeed && script.m_Name = "StoryTimelineTextClipData" then
+                                                textClipData.Add(o.m_PathID, o.ToType())
 
-                                                match textData with
-                                                | Story _ -> (-1, [ typeTree["Title"] :?> string ] :> seq<string>)
-                                                | Race _ ->
-                                                    (-1,
-                                                     (typeTree["textData"] :?> System.Collections.Generic.IList<obj>)
-                                                     |> Seq.cast<System.Collections.Specialized.OrderedDictionary>
-                                                     |> Seq.sortBy (fun obj -> obj["key"] :?> int)
-                                                     |> Seq.map (fun obj -> obj["text"] :?> string))
-                                            else
-                                                let succeed, script = o.m_Script.TryGet()
-
-                                                if succeed
-                                                   && script.m_Name = "StoryTimelineTextClipData" then
-                                                    let typeTree = o.ToType()
-                                                    let nextBlock = typeTree["NextBlock"] :?> int
-
-                                                    let result =
-                                                        [ typeTree["Name"] :?> string
-                                                          typeTree["Text"] :?> string ]
-
-                                                    let choiceDataList =
-                                                        typeTree["ChoiceDataList"]
-                                                        :?> System.Collections.Generic.IList<obj>
-
-                                                    let colorTextInfoList =
-                                                        typeTree["ColorTextInfoList"]
-                                                        :?> System.Collections.Generic.IList<obj>
-
-                                                    ((if nextBlock > 0 then
-                                                          nextBlock
-                                                      else
-                                                          Int32.MaxValue),
-                                                     result
-                                                     |> Seq.append (
-                                                         choiceDataList
-                                                         |> Seq.map (fun obj ->
-                                                             (obj :?> System.Collections.Specialized.OrderedDictionary)["Text"]
-                                                             :?> string)
-                                                     )
-                                                     |> Seq.append (
-                                                         colorTextInfoList
-                                                         |> Seq.map (fun obj ->
-                                                             (obj :?> System.Collections.Specialized.OrderedDictionary)["Text"]
-                                                             :?> string)
-                                                     ))
-                                                else
-                                                    (0, []))
-                                        |> Seq.sortBy (fun (k, _) -> k)
-                                        |> Seq.map (fun (_, v) -> v)
-                                        |> Seq.concat
-                                        |> Seq.distinct
-                                        |> Seq.map (fun text -> CppUtility.GetCppStdHash(text).ToString(), text)
+                                    let result =
+                                        match textData with
+                                        | Story _ ->
+                                            let title = mayLocalize(mainScript["Title"] :?> string)
+                                            let blockList =
+                                                (mainScript["BlockList"] :?> Collections.Generic.IList<obj>)
+                                                |> Seq.cast<Collections.Specialized.OrderedDictionary>
+                                                |> Seq.sortBy (fun obj -> obj["BlockIndex"] :?> int)
+                                                |> Seq.map (fun obj ->
+                                                    let clipList = (obj["TextTrack"] :?> Collections.Specialized.OrderedDictionary)["ClipList"] :?> Collections.Generic.IList<obj>
+                                                    if clipList.Count = 0 then
+                                                        clipList.Add(null)
+                                                    elif clipList.Count > 1 then
+                                                        raise (Exception($"Invalid block(In {data.Name})"))
+                                                    clipList
+                                                    )
+                                                |> Seq.concat
+                                                |> Seq.map (fun obj ->
+                                                    if isNull(obj) then
+                                                        ValueNone
+                                                    else
+                                                        let textClip = textClipData[(obj :?> Collections.Specialized.OrderedDictionary)["m_PathID"] :?> int64]
+                                                        let name = mayLocalize(textClip["Name"] :?> string)
+                                                        let text = mayLocalize(textClip["Text"] :?> string)
+                                                        let choiceDataList =
+                                                            textClip["ChoiceDataList"] :?> Collections.Generic.IList<obj>
+                                                            |> Seq.cast<Collections.Specialized.OrderedDictionary>
+                                                            |> Seq.map (fun obj -> mayLocalize(obj["Text"] :?> string))
+                                                            |> Seq.toArray
+                                                        let colorTextInfoList =
+                                                            textClip["ColorTextInfoList"] :?> Collections.Generic.IList<obj>
+                                                            |> Seq.cast<Collections.Specialized.OrderedDictionary>
+                                                            |> Seq.map (fun obj -> mayLocalize(obj["Text"] :?> string))
+                                                            |> Seq.toArray
+                                                        ValueSome({ Name = name; Text = text; ChoiceDataList = choiceDataList; ColorTextInfoList = colorTextInfoList })
+                                                    )
+                                                |> Seq.toArray
+                                            Some(StoryData(title, blockList))
+                                        | Race _ -> Some(RaceData(
+                                            (mainScript["textData"] :?> System.Collections.Generic.IList<obj>)
+                                            |> Seq.cast<System.Collections.Specialized.OrderedDictionary>
+                                            |> Seq.sortBy (fun obj -> obj["key"] :?> int)
+                                            |> Seq.map (fun obj -> mayLocalize(obj["text"] :?> string))
+                                            |> Seq.toArray))
 
                                     use outputFile = new FileStream(resultPath, FileMode.OpenOrCreate)
 
@@ -170,13 +227,7 @@ module Program =
                                         )
 
                                     use writer = new Utf8JsonWriter(outputFile, options)
-                                    writer.WriteStartObject()
-
-                                    for (hash, text) in textDataSeq do
-                                        writer.WritePropertyName(hash)
-                                        writer.WriteStringValue(text)
-
-                                    writer.WriteEndObject()
+                                    JsonSerializer.Serialize(writer, result)
 
                                     progressBar.Tick($"Extracted {data.Name}.json")
                     )
